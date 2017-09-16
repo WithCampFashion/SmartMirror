9e1f2e36c6dee2acd79c48ba699142f7c2c8927b
Hyuk <rlagur9286@naver.com>
2017-09-17 07:32:06 +0900

    옷 카테고리 입력 받게 수정

--- html_test/views.py
+++ html_test/views.py
@@ -9,10 +9,24 @@ def test(request):
 
 def next(request):
     query = request.GET.get('query', '')
+
+    if '가디건' in query:
+        cate = '가디건'
+    elif '남방' in query:
+        cate = '남방'
+    elif '맨투맨' in query:
+        cate = '맨투맨'
+    elif '반팔티' in query:
+        cate = '반팔티'
+    elif '트레이닝' in query:
+        cate = '트레이닝복'
+    else:
+        cate = '특이한 옷'
+
     # print(os.path.join(settings.BASE_DIR, os.path.curdir))
-    imgs = os.listdir("C:\\Users\\Hyuk\\PycharmProjects\\html_test\\html_test\\static\\img")
+    imgs = os.listdir("C:\\Users\\Hyuk\\PycharmProjects\\html_test\\html_test\\static\\img\\clothes/" + cate)
     idx = random.randrange(0, len(imgs))
-    img_path = imgs[idx]
+    img_path = "C:\\Users\\Hyuk\\PycharmProjects\\html_test\\html_test\\static\\img\\clothes/" + cate + '/' + imgs[idx]
     print(img_path)
     return render_to_response('next.html', {
         'img_path': img_path,
